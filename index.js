90cc269d8b68edfe6c88acc3e1518951ddbb7137
pavan-git <pavanwaghmare81@gmail.com>
2024-01-27 18:08:05 +0530

    new data added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+require('dotenv').config()
+
 app.get("/", (req,resp)=>{
 resp.send('Hello world')
 })
@@ -15,7 +17,7 @@ app.get('/twitter',(req,resp)=>{
 app.get('/login',(req,resp)=>{
     resp.send('<h1> Please login to the page')
 })
-app.listen(port,()=>{
+app.listen(process.env.PORT,()=>{
     console.log(`app listening on port ${port}`)
 })
 
